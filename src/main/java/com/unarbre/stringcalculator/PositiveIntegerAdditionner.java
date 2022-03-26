fa3f3efab82ce13859a13d8bc08ebba9ca1ad8cc
Simon <halimisimon@gmail.com>
2022-03-26 18:16:38 +0100

    [TEST] Step 5 : Additionner green state

--- src/main/java/com/unarbre/stringcalculator/PositiveIntegerAdditionner.java
+++ src/main/java/com/unarbre/stringcalculator/PositiveIntegerAdditionner.java
@@ -1,12 +1,28 @@
 package com.unarbre.stringcalculator;
 
+import com.unarbre.stringcalculator.exceptions.UnacceptableValueException;
 import com.unarbre.stringcalculator.structures.operator.Additionner;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PositiveIntegerAdditionner implements Additionner<CheckedInteger, Integer> {
     @Override
     public Integer add(List<CheckedInteger> checkedIntegers) {
-        return null;
+        if (this.containsNegativeValue(checkedIntegers)) {
+            throw new UnacceptableValueException("negative not allowed : " +
+                    checkedIntegers.stream()
+                            .map(CheckedInteger::getValue)
+                            .filter(n -> n < 0)
+                            .map(String::valueOf)
+                            .collect(Collectors.joining(", ")));
+        }
+
+        return checkedIntegers.stream().mapToInt(CheckedInteger::getValue).sum();
+    }
+
+
+    private boolean containsNegativeValue(List<CheckedInteger> numbers) {
+        return numbers.stream().anyMatch(n -> n.getValue() < 0);
     }
 }
