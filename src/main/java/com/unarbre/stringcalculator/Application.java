6acd7fbb4f82f2a9b234683f5b6b2322bf1e923c
Simon <halimisimon@gmail.com>
2022-03-27 18:50:01 +0200

    [ADD] Handle errors on application level

--- src/main/java/com/unarbre/stringcalculator/Application.java
+++ src/main/java/com/unarbre/stringcalculator/Application.java
@@ -5,9 +5,12 @@ import com.unarbre.stringcalculator.infrastructure.FileReader;
 
 public class Application {
     public static void main(String[] args) {
-        String userInput = new FileReader().read();
-        var stringCalculator = new StringCalculator();
-        System.out.println(userInput + " = " + stringCalculator.add(userInput));
-
+        try {
+            String userInput = new FileReader().read();
+            var stringCalculator = new StringCalculator();
+            System.out.println(userInput + " = " + stringCalculator.add(userInput));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
